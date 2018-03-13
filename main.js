44d296799698919d8118ef7a1aa9fc2d48851056
Bowen <Bowen31337@gmail.com>
2018-03-13 15:03:45 +1100

    window position

--- main.js
+++ main.js
@@ -188,12 +188,16 @@ const getWindowPosition = (mainWindow, tray) => {
 	const trayBounds = tray.getBounds()
 
 	// Center window horizontally below the tray icon
-	const x = Math.round(
+	let x = Math.round(
 		trayBounds.x + trayBounds.width / 2 - windowBounds.width / 2
 	)
 
 	// Position window 4 pixels vertically below the tray icon
-	const y = Math.round(trayBounds.y + trayBounds.height + 4)
+	let y = Math.round(trayBounds.y + trayBounds.height + 4)
+
+	if (process.platform === 'win32') {
+		y = Math.round(-trayBounds.y - trayBounds.height + 4)
+	}
 
 	return { x: x, y: y }
 }
